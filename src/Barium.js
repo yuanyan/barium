12ee9ffb7c6bf52d58dcc7603d48e5a3c390ec89
元彦 <yuanyan.cao@alibaba-inc.com>
2015-07-07 16:41:09 +0800

    Add file

---
+++ src/Barium.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var converter = require('./converter');
+
+module.exports = React.createClass({
+    propTypes: {
+        styles: React.PropTypes.object,
+        link: React.PropTypes.string
+    },
+
+    render: function() {
+
+        var component = null;
+        var styles = this.props.styles;
+        if(styles){
+
+            var stylesString = '';
+            Object.keys(styles).forEach(function(val, key){
+                var rules = styles[val];
+                stylesString += converter.rulesToString(val, rules)
+            });
+
+            component = (<style>
+             {stylesString}
+            </style>)
+        }else if(this.props.link){
+            component = (<link rel="stylesheet"href={this.prop.link} />)
+        }
+
+        return component;
+    }
+});
