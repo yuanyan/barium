c7ce0c90029e30b7520ee858d9736c0d4fae175f
元彦 <yuanyan.cao@alibaba-inc.com>
2015-07-15 01:33:46 +0800

    Update 2015-07-14T17:33:20.169Z

--- bundle.js
+++ bundle.js
@@ -1,9 +1,54 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var div = document.createElement('div');
+var prefixes = ['Moz', 'Webkit', 'O', 'ms'];
+var domVendorPrefix;
+
+// Helper function to get the proper vendor property name. (transition => WebkitTransition)
+module.exports = function (prop) {
+
+    if (prop in div.style) return prop;
+
+    var prop = prop.charAt(0).toUpperCase() + prop.substr(1);
+    if(domVendorPrefix){
+        return domVendorPrefix + prop;
+    }else{
+        for (var i=0; i<prefixes.length; ++i) {
+            var vendorProp = prefixes[i] + prop;
+            if (vendorProp in div.style) {
+                domVendorPrefix = prefixes[i];
+                return vendorProp;
+            }
+        }
+    }
+}
+
+},{}],2:[function(require,module,exports){
 var escape = require('./escape');
 var validator = require('./validator');
+var getVendorPropertyName = require('react-kit/getVendorPropertyName');
 
 var _uppercasePattern = /([A-Z])/g;
 var msPattern = /^ms-/;
+
+// Don't automatically add 'px' to these possibly-unitless properties.
+// Borrowed from jquery.
+var cssNumber = {
+  'column-count': true,
+  'fill-opacity': true,
+  'flex': true,
+  'flex-grow': true,
+  'flex-shrink': true,
+  'font-weight': true,
+  'line-height': true,
+  'opacity': true,
+  'order': true,
+  'orphans': true,
+  'widows': true,
+  'z-index': true,
+  'zoom': true
+}
 
 function hyphenateProp(string) {
   // MozTransition -> -moz-transition
@@ -15,21 +60,32 @@ function hyphenateProp(string) {
     .replace(msPattern, '-ms-');
 }
 
-function escapeValueForProp(value, prop) {
+
+function processValueForProp(value, prop) {
   // 'content' is a special property that must be quoted
   if (prop === 'content') {
     return '"' + value + '"';
   }
 
-  return escape(value);
+  // Add px to numeric values
+  if (!cssNumber[prop] && typeof value == 'number') {
+    value += 'px'
+  }else{
+    value = escape(value);
+  }
+
+  return value;
 }
 
 function ruleToString(propName, value) {
+
+  propName = getVendorPropertyName(propName);
+  
   var cssPropName = hyphenateProp(propName);
   if (!validator.isValidValue(value)) {
     return '';
   }
-  return cssPropName + ':' + escapeValueForProp(value, cssPropName) + ';';
+  return cssPropName + ':' + processValueForProp(value, cssPropName) + ';';
 }
 
 function _rulesToStringHeadless(styleObj) {
@@ -80,7 +136,7 @@ module.exports = {
   rulesToString: rulesToString
 };
 
-},{"./escape":2,"./validator":3}],2:[function(require,module,exports){
+},{"./escape":3,"./validator":5,"react-kit/getVendorPropertyName":1}],3:[function(require,module,exports){
 /**
  * Escape special characters in the given string of html.
  *
@@ -98,7 +154,19 @@ module.exports = function(html) {
     .replace(/>/g, '&gt;');
 }
 
-},{}],3:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
+module.exports = function (str) {
+  var hash = 0;
+  if (str.length == 0) return hash;
+  for (i = 0; i < str.length; i++) {
+    char = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+},{}],5:[function(require,module,exports){
 function isValidValue(value) {
   return value !== '' && (typeof value === 'number' || typeof value === 'string');
 }
@@ -110,48 +178,48 @@ module.exports = {
 },{}],"barium":[function(require,module,exports){
 var React = require('react');
 var converter = require('./converter');
+var hash = require('./hash');
 
-function hashCode(str) {
-  var hash = 0;
-  if (str.length == 0) return hash;
-  for (i = 0; i < str.length; i++) {
-    char = str.charCodeAt(i);
-    hash = ((hash << 5) - hash) + char;
-    hash = hash & hash; // Convert to 32bit integer
-  }
-  return hash;
-}
-
+var insertedRuleMap = {};
 var head = document.head || document.getElementsByTagName('head')[0];
-function insertStyle(cssText){
+var styleTag;
 
-  var styleTag = document.createElement('style')
-  styleTag.type = 'text/css'
+function appendStyle(cssText) {
+
+  if(!styleTag){
+    styleTag = document.createElement('style')
+    head.appendChild(styleTag)
+  }
 
   if (styleTag.styleSheet) {
-      styleTag.styleSheet.cssText = cssText
+    styleTag.styleSheet.cssText += cssText
   } else {
-      styleTag.appendChild(document.createTextNode(cssText))
+    styleTag.appendChild(document.createTextNode(cssText))
   }
 
-  head.appendChild(styleTag)
 }
 
 module.exports = {
-  create: function(styles){
-    var stylesString = '';
-    var stylesMap = {};
-    Object.keys(styles).forEach(function(val, key){
-        var rules = styles[val];
-        var className = '_' + hashCode(JSON.stringify(rules)); // All with ._ prefix
-        stylesMap[val] = className;
-        stylesString += converter.rulesToString('.' + className, rules);
+  create: function(styles) {
+    var cssText = '';
+    var ruleMap = {};
+
+    Object.keys(styles).forEach(function(val, key) {
+      var rules = styles[val];
+      var className = '_' + hash(JSON.stringify(rules)); // All with ._ prefix
+      var selector = '.' + className;
+      ruleMap[val] = className;
+
+      if(!insertedRuleMap[selector]){
+        cssText += converter.rulesToString(selector, rules);
+      }
+      insertedRuleMap[selector] = true;
     });
 
-    insertStyle(stylesString);
+    appendStyle(cssText);
 
-    return stylesMap;
+    return ruleMap;
   }
 }
 
-},{"./converter":1,"react":undefined}]},{},[]);
+},{"./converter":2,"./hash":4,"react":undefined}]},{},[]);
